df64f50276539a16d753fbfee9a4306f381ab37f
Robert Onisie <robert.onisie@elrond.com>
2020-04-15 18:51:01 +0300

    Update server.php

--- db/server.php
+++ db/server.php
@@ -64,7 +64,7 @@ if (isset($_POST['login_user']))
   $email = mysqli_real_escape_string($db, $_POST['email']);
   $email = filter_var($email, FILTER_SANITIZE_EMAIL);
 
-  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
 
   if (empty($email)) { array_push($errors, "Username is required"); }
   if (empty($password)) { array_push($errors, "Password is required"); }
@@ -72,7 +72,7 @@ if (isset($_POST['login_user']))
   if (count($errors) == 0) 
   {
     //$password_h = password_hash($password, PASSWORD_DEFAULT);
-    $query = "SELECT * FROM users WHERE email='$email' AND password='$password_1'";
+    $query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
     $results = mysqli_query($db, $query);
 
     if (mysqli_num_rows($results) == 1) 
