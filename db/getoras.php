97658f43d4dfcf2469bec8fda331e2b3af3d7c34
Robert Onisie <robert.onisie@elrond.com>
2020-04-15 16:55:04 +0300

    Update getoras.php

--- db/getoras.php
+++ db/getoras.php
@@ -7,16 +7,16 @@ echo $judet_num;
 $id_oras = "SELECT id from judete where name = '$judet_num'";
 $result = mysqli_query($db, $id_oras);
 
-if(!$result) echo '123';
 if($result) echo '1234';
 while($row = mysqli_fetch_array($result))
 	{
+		echo $row['id'];
 		$judet_id = $row['id'];
-		//echo $row['id'];
+		
 	}	
 
 //echo $judet_id;
-$sql = "SELECT id, name FROM orase WHERE county_id='".$judet_id."'";
+$sql = "SELECT id, name FROM orase WHERE county_id='$judet_id.";
 
 $result = mysqli_query($db, $sql);
 $oras_arr = array();
@@ -26,10 +26,10 @@ while( $row = mysqli_fetch_array($result) )
     $orasid = $row['id'];
     $oras_name = $row['name'];
 
-    echo $row['id'];
-    echo $row['name'];
+    //echo $row['id'];
+    //echo $row['name'];
 
-    $oras_arr[] = array("id" => $orasid, "name" => $oras_name);
+    $oras_arr = array('id' => $orasid, 'name' => $oras_name);
 
 }
 
