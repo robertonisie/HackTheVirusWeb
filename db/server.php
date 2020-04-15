48efdcfa8c95572b54cbc49a4604461c49e884e4
Robert Onisie <robert.onisie@elrond.com>
2020-04-15 18:57:52 +0300

    Update server.php

--- db/server.php
+++ db/server.php
@@ -1,8 +1,9 @@
  <?php
+ session_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL)
-session_start();
+
 include ("/var/db/dbconfig.php");
 include('errors.php');
 
