04092343863db261ae87e9679156f71dfa9f219b
Robert Onisie <robert.onisie@elrond.com>
2020-04-15 15:49:15 +0300

    data

--- register.php
+++ register.php
@@ -22,36 +22,24 @@ if(isset($_SESSION['nume_sesiune']))
 <script src="js/jquery-1.12.0.min.js" type="text/javascript"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script type="text/javascript">
-        $(document).ready(function(){
-
-            $("#sel_judet").change(function(){
-                var judetid = $(this).val();
-
-                $.ajax({
-                    url: '/db/getoras.php',
-                    type: "POST",
-                    data: {
-                    	judet:judetid
-                    	},
-                    dataType: 'json',
-                    success:function(response){
-
-                        var len = response.length;
-
-                        $("#sel_oras").empty();
-                        for( var i = 0; i<len; i++){
-                            var id = response[i]['id'];
-                            var name = response[i]['name'];
-
-                            $("#sel_oras").append("<option value='"+id+"'>"+name+"</option>");
-
-                        }
-                    }
-                });
-            });
-
-        });
-    </script>
+		$(document).ready(function(){
+			$("#authors").change(function(){
+				var aid = $("#authors").val();
+				$.ajax({
+					url: 'data.php',
+					method: 'post',
+					data: 'aid=' + aid
+				}).done(function(books){
+					console.log(books);
+					books = JSON.parse(books);
+					$('#books').empty();
+					books.forEach(function(book){
+						$('#books').append('<option>' + book.name + '</option>')
+					})
+				})
+			})
+		})
+	</script>
 </head>
 <body>
 	<div class="main-w3layouts wrapper">
@@ -67,27 +55,39 @@ if(isset($_SESSION['nume_sesiune']))
 					<!--<input class="text" type="text" name="judet" placeholder="Judet" required="">
 					<input class="text" type="text" name="localitate" placeholder="Localitate" required="">-->
 
-					<div>Judet</div>
-					<select id="sel_judet">
-					   <option value="0">- Select -</option>
-					   <?php 
-					   $sql_judet = "SELECT name FROM judete";
-					   $judet_data = mysqli_query($db, $sql_judet);
-
-					   while($row = mysqli_fetch_assoc($judet_data))
-					   {
-					      $judetid = $row['id'];
-					      $judet_name = $row['name'];
-					      echo "<option value='".$judetid."' >".$judet_name."</option>";
-					   }
-					   ?>
-					</select>
-					<div class="clear"></div>
-
-					<div>Oras </div>
-						<select id="sel_oras">
-						   <option value="0">- Select -</option>
-						</select>
+									<div class="container">
+						<h1 class="text-center">Dependent Drop Down list In PHP/MySQL using jQuery & Ajax </h1>
+						<hr>
+						<div class="row">
+						    <div class="col-md-6 col-md-offset-3">
+								<form role="form" method="post" action="">
+						        	<div class="row">
+						                <div class="form-group">
+						                    <label for="authors">Authors</label>
+						                    <select class="form-control" id="authors" name="authors">
+						                    	<option selected="" disabled="">Select Author</option>
+						                    	<?php 
+						                    		require 'data.php';
+						                    		$authors = loadAuthors();
+						                    		foreach ($authors as $author) {
+						                    			echo "<option id='".$author['id']."' value='".$author['id']."'>".$author['name']."</option>";
+						                    		}
+						                    	 ?>
+						                    </select>
+						                </div>
+						            </div>
+						            <div class="row">
+						                <div class="form-group">
+						                    <label for="books">Books</label>
+						                    <select class="form-control" id="books" name="books">
+						                    	
+						                    </select>
+						                </div>
+						            </div>
+						        </form>
+						    </div>
+						</div>
+					</div>
 
 					<input class="text" type="text" name="adresa" placeholder="Adresa" required="">
 					<input class="text" type="text" name="nume_afacere" placeholder="Nume Afacere" required="">
