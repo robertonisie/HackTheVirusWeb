3bfee5e59c69116b1013df0288c3b88c8dde4e6a
Robert Onisie <robert.onisie@elrond.com>
2020-04-15 15:35:59 +0300

    :(

--- db/getoras.php
+++ db/getoras.php
@@ -5,7 +5,7 @@ $judetid = $_POST['judet'];
 
 $sql = "SELECT id, name FROM orase WHERE county_id='".$judetid."'";
 
-$result = mysqli_query($sql);
+$result = mysqli_query($db, $sql);
 $oras_arr = array();
 
 while( $row = mysqli_fetch_array($result) )
