dd22d00130acde95e0c0f0545f6f460174de8525
Robert Onisie <robert.onisie@elrond.com>
2020-04-15 14:31:38 +0300

    test

---
+++ db/getoras.php
@@ -0,0 +1,21 @@
+<?php
+include ("/var/db/dbconfig.php");
+
+$judetid = 1;//$_POST['judet']; 
+
+$sql = "SELECT id,name FROM orase WHERE county_id=".$judetid;
+
+$result = mysqli_query($db, $sql);
+
+$oras_arr = array();
+
+while( $row = mysqli_fetch_array($result) ){
+    $orasid = $row['id'];
+    $oras_name = $row['name'];
+
+    $oras_arr[] = array("id" => $orasid, "name" => $oras_name);
+}
+
+echo json_encode($oras_arr);
+
+?>
