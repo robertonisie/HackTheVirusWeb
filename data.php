2ee2d1ba93107ea02c3ba90301b1bfbfb71e1ecb
Robert Onisie <robert.onisie@elrond.com>
2020-04-15 15:51:19 +0300

    123

--- data.php
+++ data.php
@@ -2,20 +2,16 @@
 	include ("/var/db/dbconfig.php");
 
 	if(isset($_POST['aid'])) {
-		$db = new DbConnect;
-		$conn = $db->connect();
 
-		$stmt = $conn->prepare("SELECT * FROM orase WHERE county_id = " . $_POST['aid']);
+		$stmt = $db->prepare("SELECT * FROM orase WHERE county_id = " . $_POST['aid']);
 		$stmt->execute();
 		$books = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		echo json_encode($books);
 	}
 
 	function loadAuthors() {
-		$db = new DbConnect;
-		$conn = $db->connect();
 
-		$stmt = $conn->prepare("SELECT * FROM judete");
+		$stmt = $db->prepare("SELECT * FROM judete");
 		$stmt->execute();
 		$authors = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		return $authors;
