864c082b73107fb96733cdfe0f7c15019e55e0d2
Robert Onisie <robert.onisie@elrond.com>
2020-04-16 15:12:20 +0300

    Update logout_product.php

--- logout_product.php
+++ logout_product.php
@@ -4,7 +4,7 @@ $pid = $_GET['id'];
     session_destroy();
     session_unset($_SESSION['nume_sesiune']);
     session_destroy();
-    header('Location: /shop/product-single?id='.$pid);
+    header('Location: /shop/product-single?id=$pid');
     exit();
 
 ?>
