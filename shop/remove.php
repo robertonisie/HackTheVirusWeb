4bae8dc6410f6e96256fb5f6ec0319b2447959b1
Robert Onisie <robert.onisie@elrond.com>
2020-04-16 19:16:46 +0300

    Update remove.php

--- shop/remove.php
+++ shop/remove.php
@@ -10,7 +10,7 @@ if(!isset($_SESSION['nume_sesiune']))
 
 $id = $_GET['id'];
 
-$sql =  "DELETE * FROM produse_cart WHERE id='$id'"; 
+$sql =  "DELETE FROM produse_cart WHERE id='$id'"; 
 $result = mysqli_query($db, $sql);
 header("Location: ../shop/cart.php");
 ?>
