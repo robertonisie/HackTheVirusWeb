fb4835b95829ad55ed025779803434bded43991b
Robert Onisie <robert.onisie@elrond.com>
2020-04-16 19:13:13 +0300

    test

---
+++ shop/remove.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+include ("/var/db/dbconfig.php");
+
+if(!isset($_SESSION['nume_sesiune']))
+{
+ header("Location: ../index.php");
+ die("Not logged in"); 
+} 
+
+$id = $_GET['id'];
+
+$sql =  "DELETE * FROM cart WHERE id='$id'"; 
+$result = mysqli_query($db, $sql);
+header("Location: ../shop/cart.php");
+?>
