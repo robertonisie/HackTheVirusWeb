4dd5cea4674f04203d4488251f8506f183430d15
Robert Onisie <robert.onisie@elrond.com>
2020-04-16 10:48:03 +0300

    asd

--- shop/product-single.php
+++ shop/product-single.php
@@ -139,16 +139,10 @@ if (isset($_POST['addtocart']))
 							<div class="w-100"></div>
 							<div class="input-group col-md-6 d-flex mb-3">
 	             	<span class="input-group-btn mr-2">
-	                	<!--<button type="button" class="quantity-left-minus btn"  data-type="minus" data-field="">
-	                   <i class="ion-ios-remove"></i>
-	                	</button>-->
 	            		</span>
                   <form method="post">
 	             	<?php echo '<input type="number" id="quantity" name="quantity" class="form-control input-number" value="1" min="1" max="'.$pcantitate.'">'; ?>
     	             	<span class="input-group-btn ml-2">
-    	                	<!--<button type="button" class="quantity-right-plus btn" data-type="plus" data-field="">
-    	                     <i class="ion-ios-add"></i>
-    	                 </button>-->
     	             	</span>
     	          	</div>
     	          	<div class="w-100"></div>
